5181a645d6154c26daefa568b10474e9bffd8c34
Ang Hao Yang <yan9za1@gmail.com>
2016-11-02 01:10:33 +0800

    Removed redundant positional argument.

--- movielens.R
+++ movielens.R
@@ -1,6 +1,6 @@
 train = read.transactions('data/movielikes_train.txt', 'single', cols = c('UserID', 'movie'))
 test = read.transactions('data/movielikes_test.txt', 'single', cols = c('UserID', 'movie'))
-rules <- apriori(train, parameter = list(supp=0.07, conf=0.95, minlen=2))
+rules <- apriori(train, list(supp=0.07, conf=0.95, minlen=2))
 
 test.list = as(test, 'list')
 test.count = length(test)
