1395f16dd791484040dec3285f580de9ae31d8f5
Ang Hao Yang <yan9za1@gmail.com>
2016-11-02 20:05:11 +0800

    Added outputs and visualisation.

--- movielens.R
+++ movielens.R
@@ -1,19 +1,30 @@
+library(arules)
+library(arulesViz)
+
 train = read.transactions('data/movielikes_train.txt', 'single', cols = c('UserID', 'movie'))
 test = read.transactions('data/movielikes_test.txt', 'single', cols = c('UserID', 'movie'))
-rules <- apriori(train, list(supp=0.07, conf=0.95, minlen=2))
-
 test.list = as(test, 'list')
 test.count = length(test)
 
-correct = 0
-total = 0
+# summary and visualisation of training transactions
+summary(train)
+itemFrequencyPlot(train, 'absolute', topN = 20)
+
+rules <- apriori(train, list(supp = 0.07, conf = 0.95, minlen = 2))
 rules.lhs = lhs(rules)
 rules.rhs = rhs(rules)
+
+print('Top 10 rules by lift:')
+inspect(head(sort(rules, by = "lift"), 10))
+
+correct = 0
+total = 0
 for (i in 1:test.count) { # for each transaction (user)
         # find rules with antecedent (LHS) in transaction
+        # also include multi antecedent rules
         is.lhs.in.tx <- is.subset(rules.lhs, test[i])
         
-        # filter movies from RHS of rules and union 
+        # filter movies from RHS of rules and union
         predictions = Reduce(union, as(rules.rhs[is.lhs.in.tx], 'list'), vector('character'))
         
         # increment correct and total predictions
