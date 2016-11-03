4dd59165d7feda2c77b05fb0aa64a7e8a793676b
Ang Hao Yang <yan9za1@gmail.com>
2016-11-03 22:06:30 +0800

    Demographics and genre.

---
+++ movielens-genre.R
@@ -0,0 +1,48 @@
+library(arules)
+library(arulesViz)
+
+train = read.transactions('data/movielikes_genre_train.txt', 'single', cols = c('UserID', 'movie'))
+test = read.transactions('data/movielikes_genre_test.txt', 'single', cols = c('UserID', 'movie'))
+test.list = as(test, 'list')
+test.count = length(test)
+
+# summary and visualisation of training transactions
+summary(train)
+itemFrequencyPlot(train, 'absolute', topN = 20)
+
+re.movie = '^m[0-9]+$'
+rules <- apriori(train, list(supp = 0.07, conf = 0.95, minlen = 2))
+rules.lhs = lhs(rules)
+rules.rhs = rhs(rules)
+rules.lhs.list = as(rules.lhs, 'list')
+rules.rhs.list = as(rules.rhs, 'list')
+#rules.lhs.movie = as(sapply(rules.lhs.list, function(x) x[grepl(re.movie, x)]), 'itemMatrix')
+
+# filter for movie only consequent
+is.rhs.movie = grepl(re.movie, rules.rhs.list)
+
+print('Top 10 rules by lift:')
+inspect(head(sort(rules, by = "lift"), 10))
+
+correct = 0
+total = 0
+for (i in 1:test.count) { # for each transaction (user)
+        # find rules with antecedent (LHS) in transaction
+        # also include multi antecedent rules
+        is.lhs.in.tx <- is.subset(rules.lhs, test[i])
+        
+        # filter movies from consequent (RHS) of rules and union
+        predictions = Reduce(union, as(rules.rhs[is.lhs.in.tx & is.rhs.movie], 'list'), vector('character'))
+        
+        # increment correct and total predictions
+        correct = correct + sum(predictions %in% test.list[[i]])
+        total = total + length(predictions)
+}
+
+precision = correct / total
+
+cat('No. of training transactions (users): ', test.count, '\n')
+cat('Correct predictions: ', correct, '\n')
+cat('Total predictions: ', total, '\n')
+cat('Precision: ', precision, '\n')
+cat('Correct predictions / transaction: ', correct / test.count, '\n')
--- preprocess.R
+++ preprocess.R
@@ -1,16 +1,88 @@
 set.seed(42)
 
-df = read.table('data/movielikes.txt', T, colClasses = 'character')
-df$movie = sapply(df$movie, function (m) if (startsWith(m, 'm')) substring(m, 2))
-users = unique(df$UserID)
+like = read.table('data/movielikes.txt', T, colClasses = 'character')
+demo = read.csv('data/u-user.csv', colClasses = 'character')
+
+users = unique(like$UserID)
 user.count = length(users)
 
 # train-test split: 80:20 of users, not records
 train.filter = sample(seq_len(user.count), size = round(user.count*.8))
 users.train = users[train.filter]
-df.train.filter = df$UserID %in% users.train
-df.train = df[df.train.filter,]
-df.test = df[!df.train.filter,]
+like.train.filter = like$UserID %in% users.train
+like.train = like[like.train.filter,]
+like.test = like[!like.train.filter,]
 
-write.table(df.train, 'data/movielikes_train.txt', sep = '\t', quote = F, row.names = F)
-write.table(df.test, 'data/movielikes_test.txt', sep = '\t', quote = F, row.names = F)
+#demographics
+demo = demo[demo$userid %in% users,]
+demo$age = sapply(demo$age, function (a) {
+        if (a < 18) 'Under 18'
+        else if (a < 25) '18-24'
+        else if (a < 35) '25-34'
+        else if (a < 45) '35-44'
+        else if (a < 50) '45-49'
+        else if (a < 56) '50-55'
+        else '56+'
+})
+demo = demo[,c('userid', 'gender', 'age', 'occupation')]
+
+temp = vector('character')
+apply(demo, 1, function (row) {
+        sapply(row[-1], function (e) {
+                temp <<- rbind(temp, c(row[1], e))
+        })
+})
+
+demo = data.frame(temp, stringsAsFactors = F)
+colnames(demo) = colnames(like)
+demo.train.filter = demo$UserID %in% users.train
+demo.train = demo[demo.train.filter,]
+demo.test = demo[!demo.train.filter,]
+
+demo.train = rbind(demo.train, like.train)
+demo.train = demo.train[order(as.numeric(demo.train$UserID)),]
+rownames(demo.train) = NULL
+demo.test = rbind(demo.test, like.test)
+demo.test = demo.test[order(as.numeric(demo.test$UserID)),]
+rownames(demo.test) = NULL
+
+# genre
+genre = read.csv('data/u_item.csv', colClasses = 'character')
+genre = genre[-(2:5)]
+genre = data.frame(sapply(genre, as.numeric))
+
+temp = vector('character')
+genre.names = colnames(genre)
+like.clean = substring(like$movie, 2)
+sapply(users, function (u) {
+        m = like.clean[like$UserID == u]
+        genre.base = genre[-(1:2)] #drop id, unknown
+        s = colSums(genre.base[genre$movie.id %in% m,])
+        
+        # top 3 genres for user
+        genre.top = names(head(sort(s[s!=0], decreasing = T), 1))
+        
+        sapply(genre.top, function(g) {
+                temp <<- rbind(temp, c(u, g))
+        })
+})
+
+genre = data.frame(temp, stringsAsFactors = F)
+colnames(genre) = colnames(like)
+genre.train.filter = genre$UserID %in% users.train
+genre.train = genre[genre.train.filter,]
+genre.test = genre[!genre.train.filter,]
+
+genre.train = rbind(genre.train, like.train)
+genre.train = genre.train[order(as.numeric(genre.train$UserID)),]
+rownames(genre.train) = NULL
+genre.test = rbind(genre.test, like.test)
+genre.test = genre.test[order(as.numeric(genre.test$UserID)),]
+rownames(genre.test) = NULL
+
+write.table(like.train, 'data/movielikes_train.txt', sep = '\t', quote = F, row.names = F)
+write.table(like.test, 'data/movielikes_test.txt', sep = '\t', quote = F, row.names = F)
+write.table(demo.train, 'data/movielikes_demo_train.txt', sep = '\t', quote = F, row.names = F)
+write.table(demo.test, 'data/movielikes_demo_test.txt', sep = '\t', quote = F, row.names = F)
+write.table(genre.train, 'data/movielikes_genre_train.txt', sep = '\t', quote = F, row.names = F)
+write.table(genre.test, 'data/movielikes_genre_test.txt', sep = '\t', quote = F, row.names = F)
